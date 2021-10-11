9aee515ec311e861036bae827dc5bed36fe9f549
wyntau <wyntau@outlook.com>
2021-10-11 20:06:20 +0800

    refactor(lib/file): update

--- src/lib/file.ts
+++ src/lib/file.ts
@@ -2,17 +2,11 @@ import debugLib from 'debug';
 import { readFile as _readFile, unlink as _unlink, writeFile as _writeFile } from 'fs';
 import { promisify } from 'util';
 
-const debug = debugLib('lint-staged:file');
+const debug = debugLib('lint-recently:file');
 const fsReadFile = promisify(_readFile);
 const fsUnlink = promisify(_unlink);
 const fsWriteFile = promisify(_writeFile);
 
-/**
- * Read contents of a file to buffer
- * @param {String} filename
- * @param {Boolean} [ignoreENOENT=true] — Whether to throw if the file doesn't exist
- * @returns {Promise<Buffer>}
- */
 export const readFile = async (filename: string, ignoreENOENT = true) => {
   debug('Reading file `%s`', filename);
   try {
@@ -27,11 +21,6 @@ export const readFile = async (filename: string, ignoreENOENT = true) => {
   }
 };
 
-/**
- * Remove a file
- * @param {String} filename
- * @param {Boolean} [ignoreENOENT=true] — Whether to throw if the file doesn't exist
- */
 export const unlink = async (filename: string, ignoreENOENT = true) => {
   debug('Removing file `%s`', filename);
   try {
@@ -45,11 +34,6 @@ export const unlink = async (filename: string, ignoreENOENT = true) => {
   }
 };
 
-/**
- * Write buffer to file
- * @param {String} filename
- * @param {Buffer} buffer
- */
 export const writeFile = async (filename: string, buffer: Buffer) => {
   debug('Writing file `%s`', filename);
   await fsWriteFile(filename, buffer);
