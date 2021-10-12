60a7168e2f997e84543813384362bee112bf70a8
wyntau <wyntau@outlook.com>
2021-10-12 12:17:14 +0800

    refactor: code cleanup

--- src/lib/symbols.js
+++ src/lib/symbols.js
@@ -3,11 +3,7 @@
 const ConfigNotFoundError = new Error('Config could not be found')
 const GetStagedFilesError = Symbol('GetStagedFilesError')
 const GitRepoError = Symbol('GitRepoError')
-const HideUnstagedChangesError = Symbol('HideUnstagedChangesError')
 const InvalidOptionsError = new Error('Invalid Options')
-const RestoreMergeStatusError = Symbol('RestoreMergeStatusError')
-const RestoreOriginalStateError = Symbol('RestoreOriginalStateError')
-const RestoreUnstagedChangesError = Symbol('RestoreUnstagedChangesError')
 const TaskError = Symbol('TaskError')
 
 module.exports = {
@@ -15,9 +11,5 @@ module.exports = {
   GetStagedFilesError,
   GitRepoError,
   InvalidOptionsError,
-  HideUnstagedChangesError,
-  RestoreMergeStatusError,
-  RestoreOriginalStateError,
-  RestoreUnstagedChangesError,
   TaskError,
 }
